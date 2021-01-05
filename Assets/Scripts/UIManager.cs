96a9e3abf6a0eda30ad318d18ea47bafb2dc656e
Alex Semchenkov <zigola12@list.ru>
2021-01-05 16:32:34 +0300

    Some function's of UIManager
    Made load screen. Implemented function to go next level(named NextLevel).

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Image fillground;
+    private string _nameUI;
+    public string nameUI
     {
-        
+        get {return _nameUI;}
+        set { _nameUI = value;}
     }
 
-    // Update is called once per frame
+    string simpleVariable;
+
+    
     void Update()
     {
-        
+        if (Input.GetKeyDown("f"))
+        {
+            StartCoroutine("FirstLoad");
+        }
+        if (_nameUI != null)
+        {
+            NextLevel(_nameUI);
+        }
     }
+    IEnumerator FirstLoad()
+    {
+        for (float ft = fillground.fillAmount; ft <= 1; ft += 0.0025f)
+        {
+            fillground.fillAmount += ft;
+            yield return new WaitForSeconds(.1f);
+            if(fillground.fillAmount == 1)
+            {
+                GameObject Namelevel = GameObject.Find("[Interface]/CanvasRoot/alertPanel");
+                simpleVariable = Namelevel.name;
+                Debug.Log(simpleVariable);
+                Namelevel.SetActive(true);
+                Namelevel = GameObject.Find("[Interface]/CanvasRoot/loadPanel");
+                Namelevel.SetActive(false);
+                StopAllCoroutines();
+                
+            }
+        }
+    }
+
+    void NextLevel(string name)
+    {
+        GameObject Namelevel = GameObject.Find("[Interface]/CanvasRoot/" + simpleVariable);
+        Namelevel.SetActive(false);
+        Namelevel = GameObject.Find("[Interface]/CanvasRoot/"+ name );
+        Namelevel.SetActive(true);
+        simpleVariable = name;
+
+
+        _nameUI = null;
+        Debug.Log(simpleVariable);
+    }
+    
 }
