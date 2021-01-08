5da6e8c80ee65d0bd046ce46fe382bce8219752e
Alex Semchenkov <zigola12@list.ru>
2021-01-08 17:13:58 +0300

    Fix bags

---
+++ Assets/Scripts/infoTheme.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Flags]
+public enum Infosrc
+{
+    first = 1,
+    second,
+    third
+}
+public class infoTheme : MonoBehaviour
+{
+    public Infosrc info;
+    public GameObject qwe;
+    public GameObject ewq;
+    
+    public void Chadfsa(Infosrc info)
+    {
+        switch (info)
+        {
+            case Infosrc.first:
+                qwe.SetActive(true);
+                break;
+            case Infosrc.second:
+                ewq.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+    
+}
