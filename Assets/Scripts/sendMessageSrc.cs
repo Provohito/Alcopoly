3b67745e7c53f89dfb642ecfd91582cf0668b8a8
Alex Semchenkov <zigola12@list.ru>
2021-01-08 15:17:13 +0300

    Made sendmessage function(Write in file.txt)

---
+++ Assets/Scripts/sendMessageSrc.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class sendMessageSrc : MonoBehaviour
+{
+    [SerializeField]
+    private Text namePlayer;
+    [SerializeField]
+    private Text emailPlayer;
+    [SerializeField]
+    private Text messagePlayer;
+
+
+    
+    public void SendMessage()
+    {
+        string s = "Name: " + namePlayer.text + "\n" + "Email: " + emailPlayer.text + "\n" + "Message: " + messagePlayer.text + "\n" + "Текущее время: " + DateTime.Now + "\n";
+       
+        using (StreamWriter sw = File.AppendText(@"D:\Work\Alcopoly\Assets\Resources\sendNotes.txt"))
+        {
+            sw.WriteLine(s);   
+        }
+    }
+
+}
+    
+
+    
+
